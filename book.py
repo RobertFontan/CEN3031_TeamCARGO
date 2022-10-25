8dc8a4066397e56052bb7ff392dce4a45344349b
parzivail <colby.newman2000@gmail.com>
2022-10-25 13:33:53 -0400

    ISBN lookup skeleton

---
+++ book.py
@@ -0,0 +1,7 @@
+class Book:
+  def __init__(self, isbn, title, author, rating, genre, coverUrl):
+    self.isbn = isbn
+    self.title = title
+    self.rating = rating
+    self.genre = genre
+    self.coverUrl = coverUrl
---
+++ isbn.py
@@ -0,0 +1,4 @@
+from book import Book
+
+def lookup_isbn(isbn):
+    return Book("978-1250037756", "Mr. Penumbra's 24-Hour Bookstore", "Robin Sloan", 3.7, "novel", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1345089845i/13538873.jpg")
