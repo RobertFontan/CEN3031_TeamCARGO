e295196a2e0f9fdfc0d84f76e22bdf0a7d7e7694
parzivail <colby.newman2000@gmail.com>
2022-10-28 12:00:08 -0400

    incorporate rob's ISBN API into the lookup framework

--- book.py
+++ book.py
@@ -2,6 +2,7 @@ class Book:
   def __init__(self, isbn, title, author, rating, genre, coverUrl):
     self.isbn = isbn
     self.title = title
+    self.author = author
     self.rating = rating
     self.genre = genre
     self.coverUrl = coverUrl
--- isbn.py
+++ isbn.py
@@ -1,4 +1,9 @@
+import json
+from urllib.request import urlopen
 from book import Book
 
 def lookup_isbn(isbn):
-    return Book("978-1250037756", "Mr. Penumbra's 24-Hour Bookstore", "Robin Sloan", 3.7, "novel", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1345089845i/13538873.jpg")
+    resp = urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn)
+    respJson = json.load(resp)
+    bookInfo = respJson['items'][0]['volumeInfo']
+    return Book(bookInfo['industryIdentifiers'][1]['identifier'], bookInfo['title'], bookInfo['authors'][0], bookInfo['averageRating'], bookInfo['categories'][0], bookInfo['imageLinks']['thumbnail'])
