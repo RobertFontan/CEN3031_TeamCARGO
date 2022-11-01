b0287631a05aedc396f26e039843d779c2085cca
Robert Fontan <robertfontan@gmail.com>
2022-11-01 15:04:38 -0400

    working

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from isbn import lookup_isbn
 import book
 
 # this does not work
+bookList = []
+
 while True:
     isbn = input("Enter an ISBN: ").strip()
     lookup_isbn(isbn)
-
-    bookList = []
+    
     bookList.append(lookup_isbn)
 
     user_update = input("Want another ISBN? [Y/N] ").lower().strip()
@@ -15,3 +16,9 @@ while True:
         break 
     
 print(len(bookList))
+
+
+# Harry Potter 0439708184
+# the stranger 9780394533056
+# Game of Thrones 9780553593716
+
