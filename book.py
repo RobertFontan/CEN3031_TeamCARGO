0200451a27b36313e7c32773555263fc0062f288
Robert Fontan <robertfontan@gmail.com>
2022-11-01 13:32:44 -0400

    together

--- book.py
+++ book.py
@@ -1,3 +1,8 @@
+import json
+from urllib.request import urlopen
+from book import Book
+
+
 class Book:
   def __init__(self, isbn, title, author, rating, genre, coverUrl):
     self.isbn = isbn
@@ -6,3 +11,10 @@ class Book:
     self.rating = rating
     self.genre = genre
     self.coverUrl = coverUrl
+    
+
+  def lookup_isbn(isbn):
+      resp = urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn)
+      respJson = json.load(resp)
+      bookInfo = respJson['items'][0]['volumeInfo']
+      return Book(bookInfo['industryIdentifiers'][1]['identifier'], bookInfo['title'], bookInfo['authors'][0], bookInfo['averageRating'], bookInfo['categories'][0], bookInfo['imageLinks']['thumbnail'])
