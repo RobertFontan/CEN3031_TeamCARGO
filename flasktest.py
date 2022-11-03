8e7f294cd3967ca130b15ebda0814ac5b48a2695
RobertFontan <87390691+RobertFontan@users.noreply.github.com>
2022-11-03 19:43:06 -0400

    flask working looks like garbage

--- flasktest.py
+++ flasktest.py
@@ -1,70 +1,40 @@
+from urllib import response
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import requests
+import json
+from urllib.request import urlopen
 
-from pyparsing import nullDebugAction
+
 
 from book import Book, lookup_isbn
 
 app = Flask(__name__)  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
 
-class Library(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.String(10), nullable= False)
-    #title = db.Column(db.String(200), nullable= False)
-    #coverUrl = db.Column(db.String(200), nullable = False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+def get_book():
+    url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+    response = json.loads(requests.request("GET", url).text)
+    isbn = str(9780553593716)
+    resp = urlopen(url + isbn)
+    bookObject = json.load(resp)
+    bookInfo = bookObject['items'][0]['volumeInfo']
     
-    def __repr__(self):
-        return '<Book %r>' % self.id
+    author = str(bookInfo['authors'][0])
+    title = str(bookInfo['title'])
+    coverUrl = bookInfo['imageLinks']['smallThumbnail']
+        
     
-    
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    
-@app.route('/', methods=['POST', 'GET'])
+    return author, title, coverUrl
+
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        newISBN = request.form['isbn']
-        
-   #     nB = lookup_isbn(str(newISBN))
-        
-        
-        newBook = Library(isbn = newISBN)
-     #   newCoverURL = Library(coverUrl = nB.getCover())
-        
-        try:
-            db.session.add(newBook)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your book'
-            
-    else:
-        books = Library.query.order_by(Library.date_created).all()
-        return render_template('index.html', books=books)
-            
+    author, title, coverURL = get_book()
+    return render_template("index.html", author=author,title=title, coverURL=coverURL)
+
             
 if __name__ == "__main__":
     app.run(debug=True)
             
             
-    # __tablename__ = 'books'
-    # isbn = db.Column(db.Integer, primary_key=True)
-    # title = db.Column(UnicodeText)
-    # author = db.Column(UnicodeText)
-    # rating = db.Column(UnicodeText)
-    # genre = db.Column(UnicodeText)
-    # coverurl = db.Column(UnicodeText)
-
-    # def __init__(self, book):
-    #     self.isbn = book.isbn
-    #     self.title = book.title
-    #     self.author = book.author
-    #     self.rating = book.rating
-    #     self.genre = book.genre
-    #     self.coverUrl = book.coverUrl
