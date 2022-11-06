be4ff7181c9ebe514554ecb70bc3b21cd0e07b25
Robert Fontan <robertfontan@gmail.com>
2022-11-05 20:48:01 -0400

    working main library gui

--- isbngui.py
+++ isbngui.py
@@ -19,11 +19,6 @@ class GUI:
         self.textbox = tk.Text(self.root, height=1, width=50, font=('Arial', 16))
         self.textbox.pack(padx=10, pady=10)
         
-        # self.check_state = tk.IntVar()
-        
-        # self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=('Arial', 18), variable=self.check_state)
-        # self.check.pack(padx=10, pady=10)
-        
         self.frame = tk.Frame(self.root)
         self.frame.pack()
         
@@ -37,10 +32,20 @@ class GUI:
         self.root.mainloop()
         
     def add_cover(self):
+        
         new_isbn = self.textbox.get('1.0', tk.END)
         new_cover = lookup_cover(new_isbn)        
         #print(new_cover)
-           
+        
+        tempList = ['9780394533056',
+                    '9780553593716',
+                    '0735219095',
+                    '1791392792',
+                    '1501161938',
+                    '0062653318',
+                    '1538719843']
+        
+        
         u = urlopen(new_cover)
         raw_data = u.read()
         u.close()
@@ -48,20 +53,17 @@ class GUI:
         self.bookGrid.configure(image=image)
         self.bookGrid.image = image 
         
-        #mg = tk.Label(self.bookGrid,image=photo, text= new_isbn, font=('Arial', 18))
+        # mg = tk.Label(self.bookGrid,image=photo, text= new_isbn, font=('Arial', 18))
         
         # self.newBook.grid(row=1, column=1, sticky=tk.W +tk.E)
-        #self.newBook.place(x=200, y= 200)
+        # self.newBook.place(x=200, y= 200)
         # self.newBook.grid(row = 0, column=0, sticky=tk.W + tk.E)
        
         
-       # the stranger 9780394533056
+        # the stranger 9780394533056
         # Game of Thrones 9780553593716
 
-        
-        
-    
-            
+
         
     # def show_message(self):
     #     if self.check_state.get() == 0:
