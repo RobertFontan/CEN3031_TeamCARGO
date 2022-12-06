0a8f9058d24939b4d2760172b2f4a53524ddb7d2
Robert Fontan <robertfontan@gmail.com>
2022-12-06 16:16:30 -0500

    Added design elements

--- mainlibrarygui.py
+++ mainlibrarygui.py
@@ -33,35 +33,41 @@ class LibraryModel(object):
 
 libModel = LibraryModel()
 
-#coverList = []
-#favList = []
-
-
-
 class mainLibGUI(tk.Tk):
     def __init__(self):
         super().__init__()
-        
+        # intializes the main gui        
         self.geometry("800x800")
         self.title("Encyclomedia")
         
         
         #adds a background
         bg2 = PhotoImage(file="images/libraryBR.png")
-        #set and add backround
-        gui_bk = Label(self, image = bg2, bg="black")
-        gui_bk.place(x=0, y=0, relwidth=1, relheight=1)
         
         
+        # set and add background
+        canvas = Canvas(self, width=2000, height=2000 )
+        canvas.create_image(400,400, anchor=CENTER, image = bg2)
+        canvas.create_text(115,40, anchor=CENTER, text="Bookshelf", font=('Arial', 30, 'bold'), fill = "black")
+
+        canvas.place(x=0, y=0, relwidth=1, relheight=1)
+       
+   
+        label = Label(self)
+        label.pack(padx=20, pady=30)
+        
+        
+        
+        self.wm_attributes("-transparentcolor", "red")
+        # established some main library buttons
         isbnButton = tk.Button(self, text="Add a book", font=('Arial', 18), command=self.addIsbn)
-        isbnButton.pack(padx=10, pady=10)
+        isbnButton.place(x=390, y =20)
         
-        achButton = tk.Button(self, text= "View Statisics", font=('Arial', 18), command=self.viewStats)
-        achButton.pack()
+        statButton = tk.Button(self, text= "View Statisics", font=('Arial', 18), command=self.viewStats)
+        statButton.place(x=600, y= 20)
         
-        label = Label(self, text="Your Bookshelf", font=('Arial', 18))
-        label.pack(padx=20, pady=20)
         
+        # positions the grid system for book in book shelf
         bookFrame = Frame(self, bg="brown")
         bookFrame.pack()
         
@@ -72,76 +78,87 @@ class mainLibGUI(tk.Tk):
         bookFrame.columnconfigure(3)
         bookFrame.columnconfigure(4)
 
-        self.book0 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book0.grid(row=0, column=0, )
+        self.book0 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book0.grid(row=0, column=0, padx= 10, pady=10)
 
-        self.book1 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book1.grid(row=0, column=1)
+        self.book1 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book1.grid(row=0, column=1, padx = 10)
 
-        self.book2 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book2.grid(row=0, column=2)
+        self.book2 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book2.grid(row=0, column=2, padx= 10)
 
-        self.book3 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book3.grid(row=0, column=3)
+        self.book3 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book3.grid(row=0, column=3,padx= 10)
         
-        self.book4 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book4.grid(row=0, column=4)
+        self.book4 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book4.grid(row=0, column=4, padx=10)
         
-        self.book5 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book5.grid(row=1, column=0)
+        self.book5 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book5.grid(row=0, column=5, padx=10)
         
-        self.book6 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book6.grid(row=1, column=1)
+        self.book6 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book6.grid(row=1, column=0, padx=10)
         
-        self.book7 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book7.grid(row=1, column=2)
+        self.book7 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book7.grid(row=1, column=1, padx=10)
         
-        self.book8 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book8.grid(row=1, column=3)
+        self.book8 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book8.grid(row=1, column=2, padx=10)
         
-        self.book9 = tk.Button(bookFrame, bg="brown", command=self.goalBox)
-        self.book9.grid(row=1, column=4)
+        self.book9 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book9.grid(row=1, column=3, padx=10)
         
-        #self.book10 = tk.Button(bookFrame)
+        self.book10 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book10.grid(row=1, column=4, padx=10)
+
+        self.book11 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book11.grid(row=1, column=5, padx=10)
+
+        self.book12 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book12.grid(row=2, column=0, padx=10)
+
+        self.book13 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book13.grid(row=2, column=1, padx=10)
         
+        self.book14 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book14.grid(row=2, column=2, padx=10)
+
+        self.book15 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book15.grid(row=2, column=3, padx=10)
+
+        self.book16 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book16.grid(row=2, column=4, padx=10)
+
+        self.book17 = tk.Button(bookFrame, bg="brown", command=self.goalBox, borderwidth='0')
+        self.book17.grid(row=2, column=5, padx=10)
+
+        
+        # added variables in charge of statistics
         self.daily = tk.IntVar()
         self.completed = tk.IntVar()
         
         self.bookCount = tk.IntVar(self, value=0, name = "bookCount")
 
-        bookFrame.pack(fill='x')
-        
+        bookFrame.pack(padx =10, fill='x')
+   
+
+        # adds existing books
         for book in libModel.get_books():
             self.addCover(book)
         
         self.mainloop()
 
     
-
+    #funciton for adding a new book cover image to ui
     def addCover(self, newBook):
         bookCounter = self.bookCount.get()
-         
-        #cv = coverList[bookCounter]
-        print(newBook.getISBN())
-        
         newCover = newBook.getCover()
-        
-        print(newCover)        
-        #print("from the book object" + newCover)
-        #print(cv)
-        
-        # u = urlopen(newCover)
-        # raw_data = u.read()
-        # u.close()
-        # image = ImageTk.PhotoImage(data=raw_data)
-        
-        # reponse = requests.get(newCover)
-        # img_data = reponse.content        
-        # image = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
-        
-        raw_data = urllib.request.urlopen(newCover).read()
-        im = Image.open(io.BytesIO(raw_data))
-        image = ImageTk.PhotoImage(im)
+        reponse = requests.get(newCover)
+        img_data = reponse.content     
+        img = Image.open(BytesIO(img_data))
+        img = img.resize((100, 180))   
+        image = ImageTk.PhotoImage(img)
+       
         
         if bookCounter == 0:
             self.book0.configure(image=image)
@@ -172,16 +189,38 @@ class mainLibGUI(tk.Tk):
             self.book8.image = image
         elif bookCounter == 9:
             self.book9.configure(image=image)
-            self.book9.image = image                
+            self.book9.image = image
+        elif bookCounter == 10:
+            self.book10.configure(image=image)
+            self.book10.image = image
+        elif bookCounter == 11:
+            self.book11.configure(image=image)
+            self.book11.image = image
+        elif bookCounter == 12:
+            self.book12.configure(image=image)
+            self.book12.image = image
+        elif bookCounter == 13:
+            self.book13.configure(image=image)
+            self.book13.image = image
+        elif bookCounter == 14:
+            self.book14.configure(image=image)
+            self.book14.image = image
+        elif bookCounter == 15:
+            self.book15.configure(image=image)
+            self.book15.image = image
+        elif bookCounter == 16:
+            self.book16.configure(image=image)
+            self.book16.image = image
+        elif bookCounter == 17:
+            self.book17.configure(image=image)
+            self.book17.image = image
         else:
-            pass
-            
+            messagebox.showwarning(title="Error", message="Not enough books")
+                    
         bookCounter = bookCounter + 1
-            
         self.bookCount.set(bookCounter)
             
-        #    self.bookCount = self.bookCount.get() + 1 
-    
+    # prompts user to add isbn of book they want to add and calls to add the cover
     def addIsbn(self):
         isbn = simpledialog.askstring("Input", "Add a ISBN",
                                  parent=self)
@@ -194,6 +233,7 @@ class mainLibGUI(tk.Tk):
             newBook = lookup_isbn(isbn)
             libModel.add_book(newBook)
             self.addCover(newBook)
+        
         except Exception:
             messagebox.showwarning(title="Error", message="Couldn't find the ISBN!")
 
@@ -227,8 +267,7 @@ class mainLibGUI(tk.Tk):
         print("daily is : " + str(self.daily.get()))
         print("completed is : " + str(self.completed.get()))
         
-        
-    
+    # when clicking a book gives dialog to update it
     def goalBox(self):
         goalBox = Toplevel()
         goalBox.title('Book Options')
