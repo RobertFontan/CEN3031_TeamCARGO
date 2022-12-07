b0742b54a10f249822f579b3c2340d0ea3d340c2
parzivail <colby.newman2000@gmail.com>
2022-12-06 22:20:28 -0500

    comments

--- bookdb.py
+++ bookdb.py
@@ -8,6 +8,7 @@ from book import Book
 engine = create_engine('sqlite:///library.db', echo=False)
 Base = declarative_base(bind=engine)
 
+# Object that stores the necessary metadata for database serialization
 class SerializedBook(Base):
     __tablename__ = 'books'
     isbn = Column(Integer, primary_key=True)
@@ -32,20 +33,25 @@ class SerializedBook(Base):
 
 Base.metadata.create_all()
 
+# Create a database connection
 def create_session():
     Session = sessionmaker(bind=engine)
     return Session()
 
+# Add multiple books to the database
 def add_books(session, books):
     session.add_all([SerializedBook(b) for b in books])
     session.commit()
 
+# Add one book to the database
 def add_book(session, book):
     add_books(session, [book])
 
+# Get all books in the database
 def get_books(session):
     return [b.deserialize() for b in session.query(SerializedBook)]
 
+# Remove all instances of an ISBN from the database
 def remove_book(session, isbn):
     session.query(SerializedBook).filter_by(isbn=isbn).delete()
     session.commit()
